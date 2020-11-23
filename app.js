a9977e435d358277aea85e14b44b6f1efec690eb
Jorge Gallegos <gallegosjorge403@gmail.com>
2020-11-23 17:31:29 -0600

    Integrated Socket.io

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const app = express()
 
 const server = require('http').createServer(app)
 
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+  console.log("🔌 New user connected! 🔌");
+})
+
 const exphbs  = require('express-handlebars');
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs({
+  defaultLayout: null
+}));
 app.set('view engine', 'handlebars');
+
+app.use('/public', express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index.handlebars');
