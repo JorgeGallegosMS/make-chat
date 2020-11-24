cd88fb6d8856123772978bddf20d3736bdde2bc3
Jorge Gallegos <gallegosjorge403@gmail.com>
2020-11-23 19:13:04 -0600

    Fixed channel persistence

--- public/index.js
+++ public/index.js
@@ -73,9 +73,15 @@ $(document).ready(()=>{
   })
 
   //socket listeners
-  socket.on('new user', (username) => {
+  socket.on('new user', ({username, channels}) => {
     console.log(`${username} has joined the chat`);
     $('.users-online').append(`<div class="user-online">${username}</div>`);
+    for (channel in channels) {
+      if (channel === "General") {
+        continue
+      }
+      $('.channels').append(`<div class="channel">${channel}</div>`);
+    }
   })
 
   socket.on('new message', (data) => {
